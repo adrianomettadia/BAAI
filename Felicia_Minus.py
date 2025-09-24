6c54e9834efcaeff2f89e90622cecd273de8db78
Felicia <adriano.mettadia@gmail.com>
2025-09-24 16:49:58 +0800

    created two files.

---
+++ Felicia_Minus.py
@@ -0,0 +1,18 @@
+#
+# Felicia, 2025/09/24
+# File: Felicia_Minus.py
+# Short description of the task
+#
+
+# 1. Input
+X = 8
+Y = 7
+
+# 2. Process
+Sum = X - Y
+
+# 3. Output
+print(f'Result {Sum}')
+# 2. Process
+
+# 3. Output
--- Felicia_sum.py
+++ Felicia_sum.py
@@ -1,11 +1,15 @@
 #
 # Felicia, 2025/09/24
 # File : Felicia_Sum.py
-# Short description of the task
+# Add two numbers then print
 #
 
 # 1. Input
+X = 8
+Y = 7
 
 # 2. Process
+Sum = X + Y
 
 # 3. Output
+print(f'Result {Sum}')
